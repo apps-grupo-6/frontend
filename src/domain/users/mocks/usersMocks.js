cf8a32f032df2cd44441db8659aa0d27b4cd6d40
Francisco Sole <103468460+FranciscoSole@users.noreply.github.com>
2025-11-13 14:22:02 -0300

    Delete src/domain/users/mocks/usersMocks.js

--- src/domain/users/mocks/usersMocks.js
+++
@@ -1,8 +0,0 @@
-export const me = {
-  id: 1,
-  username: "usuario.demo",
-  first_name: "Santiago",
-  last_name: "Pérez",
-  contact_email: "santiago@example.com",
-  telephone: "1122334455",
-};
