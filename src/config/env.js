0fdf144db469866ccc37842a50cb490319a045f3
Francisco Sole <103468460+FranciscoSole@users.noreply.github.com>
2025-11-13 14:18:26 -0300

    Delete src/config/env.js

--- src/config/env.js
+++
@@ -1,7 +0,0 @@
-import Constants from "expo-constants";
-
-const extra = Constants.expoConfig.extra;
-export default {
-  apiUrl: extra.API_URL,
-  useMocks: String(extra.USE_MOCKS).toLowerCase() === "true",
-};
